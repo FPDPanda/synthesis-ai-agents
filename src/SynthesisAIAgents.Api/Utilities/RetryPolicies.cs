b5875613ce1598592047cfd94939e4cafc359eec
Jean Jacques <35910266+FPDPanda@users.noreply.github.com>
2025-11-02 00:13:08 -0300

    Adding RetryPolicies

---
+++ src/SynthesisAIAgents.Api/Utilities/RetryPolicies.cs
@@ -0,0 +1,10 @@
+﻿using Polly;
+
+namespace SynthesisAIAgents.Api.Utilities
+{
+    public class RetryPolicies
+    {
+        public static IAsyncPolicy BasicRetry(int retries = 2, int delayMs = 500) =>
+            Policy.Handle<Exception>().WaitAndRetryAsync(retries, i => TimeSpan.FromMilliseconds(delayMs));
+    }
+}
